a8890fd7f4a959afb8d35936b4d72ec8fccecaec
Arsenyeva Olga <shal-ol@mail.ru>
2019-07-11 17:14:55 +0300

    parallax, 2 sliders, skills and adaptive added

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,44 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+const skill = {
+  template: "#skills-circle",
+  props:{
+    skillName: String,
+    skillPersent: Number
+  },
+  mounted() {
+    const circle = this.$refs['circle'];
+    const dashArray = parseInt(
+      getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+    );
+    const percent = (dashArray / 100) * (100 - this.skillPersent);
+    circle.style.strokeDashoffset = percent;
+  }
+}
+
+const row = {
+  template: "#skills-block",
+  props:{
+    skill: Object
+  },
+  components: {
+    skill
+  }
+}
+
+new Vue ({
+  el: "#skills-container",
+  template: "#skills-list",
+  components: {
+    row
+  },
+  data() {
+    return{
+      skills: []
+    }
+  },
+  created() {
+    const data = require('../data/skills.json');
+    this.skills = data;
+  }
+})
